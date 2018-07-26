1c9bf2c4c4da24644ec81ebdfcc5d1855a3b4ef0
ParkerCarnahan <40832646+ParkerCarnahan@users.noreply.github.com>
2018-07-26 13:00:44 -0600

    Update sketch.js

--- Arrays/gravity-bouncer-basic-master/sketch.js
+++ Arrays/gravity-bouncer-basic-master/sketch.js
@@ -4,6 +4,8 @@
 // Declare Global Variables
 
 let balls;
+let player;
+let count = 0;
 
 let GREY = 80;
 let RED = "#bb2a27";
@@ -17,6 +19,10 @@ function setup() {
 	createCanvas(800, 600);
 	
 	// Initialize Global Variables
+	player = []
+	for (let n = 0; n < 1 ; n++){
+		player.push(new jumpingPlayer(200, 580, BLUE, 65, 68, 87));
+	}
 	balls = [];
 	for (let n = 0; n < 1 ; n++){
 		balls.push(new ball(random(800), random(600)));
@@ -27,6 +33,11 @@ function setup() {
 function draw() {
 	background("orange");
 
+	for(let i = 0; i < player.length; i++){
+		player[i].show();
+		player[i].move();
+	}
+
 	for(let i = 0; i < balls.length; i++){
 		balls[i].move();
 		balls[i].show();
@@ -35,21 +46,31 @@ function draw() {
 	if (balls.length > 250){
 		balls.splice(i, 1)
 	}
+
+	for(let i = 0; i < player.length; i++){
+		if (player[i].click(player[i].x,player[i].y)){
+			count + 1;
+		}
+	}
+}
 }
 
-}
 
 function keyPressed() {
+	if (keyCode == 49){
 	balls.push(new ball(mouseX, mouseY));
+	}
 }
 
 function mousePressed(){
+
 	for(let i = 0; i < balls.length; i++){
 	if (balls[i].clicked(mouseX, mouseY)){
 		balls.splice(i, 1);
-		
+		balls.push(new ball(random(800), random(600)))
+		balls.push(new ball(random(800), random(600)))
+
 	}
 }
 
 }
-
